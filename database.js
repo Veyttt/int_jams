1acb873d85150d87eb345f29c8a8a8038e1be31a
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2024-06-11 16:17:33 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 't32.h.filess.io', 
-    user: 'Interstellarjams_timesoonif', 
+    user: 'Interstellarjams_timesoonif',
+    port: 3307,
     password: 'ac67029e7f39d25d3d2add03528516d9c5a448d2', 
     database: 'Interstellarjams_timesoonif' 
 });
