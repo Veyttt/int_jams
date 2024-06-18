d9e80a93bc6412986a1e9a66abf30eb5f6772862
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2024-06-18 20:37:08 +0100

    Update Preload.js

--- www/Engine/src/scenes/Preload.js
+++ www/Engine/src/scenes/Preload.js
@@ -63,6 +63,8 @@ class Preload extends Phaser.Scene {
 		
 
 		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Level"));
+		
+		this.load.audio('cassette1sound', 'types/cassette1.mp3');
 	}
 
 	/* END-USER-CODE */
